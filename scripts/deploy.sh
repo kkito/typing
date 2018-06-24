65d5e29df10a05fa82d9fb06fce5391028464621
kkito <kkito@sohu.com>
2018-06-24 11:00:23 +0800

    deploy to server

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,6 @@
 
-ls -ltrh
-#  ls -ltrh dist/
-#  eval "$(ssh-agent -s)"
-#  chmod 600 ./ciweb
-#  ssh-add ./ciweb
-#  rsync -e "ssh -o StrictHostKeyChecking=no" -avc dist/ ciweb@kkito.cn:/var/www/html/typing/
+ ls -ltrh dist/
+ eval "$(ssh-agent -s)"
+ chmod 600 ./ciweb
+ ssh-add ./ciweb
+ rsync -e "ssh -o StrictHostKeyChecking=no" -avc dist/ ciweb@kkito.cn:/var/www/html/typing/
